5d6bfd2e3c736e23b40dda8d2199bd20078a6d5c
cesarvega <cesarvega.col@gmail.com>
2019-08-30 15:48:46 -0400

    profile updatedone

--- src/app/main/user-info/profile/profile.service.ts
+++ src/app/main/user-info/profile/profile.service.ts
@@ -9,6 +9,8 @@ import { AuthService } from 'app/main/services/auth/auth.service';
 export class ProfileService {
 
   profile$: Observable<any>;
+  user$: Observable<any>;
+  pictURL;
 
   constructor(
     private firestore: AngularFirestore,
@@ -17,17 +19,17 @@ export class ProfileService {
 
   createprofile(profile): Observable<any> {
     if (!this.profile$) {
-      this._auth.getuser().subscribe(user => {
+      this.user$.subscribe(user => {
         this.firestore.collection('profiles', ref => ref.where('uid', '==', user.uid))
           .snapshotChanges()
           .subscribe(profileExist => {
-            if (profileExist.length) {                 
-              this.setProfile(profileExist);            
+            if (profileExist.length) {
+              this.setProfile(profileExist);
               return profileExist;
-            } else {              
-            return this.firestore
-            .collection('profiles')
-            .add(profile);
+            } else {
+              return this.firestore
+                .collection('profiles')
+                .add(profile);
             }
           });
 
@@ -40,20 +42,33 @@ export class ProfileService {
   Getprofile(): Observable<any> {
     return this.firestore.collection('profiles').snapshotChanges();
   }
-
-  update(profile): Promise<any> {
-    const profileId: any = this.profile$.subscribe(x => {
-      return x;
-    });
-    return this.firestore.doc('profiles/' + profileId).update(profile);
+// 
+  update(profile): void {     
+    const remappedProfile = {
+      firstName: profile['First name *'],
+      middleName: profile['Middle name'],
+      lastName: profile['Last name *'],
+      gender: profile['First name *'],
+      dob: profile['Date of birth *'],
+      contry: profile['Select country of residency *'],
+      address: profile['Mailing street address 1 *'],
+      address2: profile['Mailing street address 2'],
+      state: profile['Select State/Province *'],
+      zipcode: profile['Zip Code *'],
+      phone: profile['Cell Phone'],
+      profession: profile['Specify your profession or occupation in order to continue'],
+    };
+    this.firestore.doc('profiles/' + this.profile$[0].payload.doc.id).update(remappedProfile);
   }
 
-  removeprofile(profile): Promise<any> {
-    delete profile.id;
-    return this.firestore.doc('profiles/' + profile.id).delete();
-  }
 
-  setProfile(profile): void {
-    this.profile$ = profile;
-  }
+removeprofile(profile): Promise < any > {
+  delete profile.id;
+
+  return this.firestore.doc('profiles/' + profile.id).delete();
+}
+
+setProfile(profile): void {
+  this.profile$ = profile;
+}
 }
